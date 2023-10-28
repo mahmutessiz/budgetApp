abc0ad8d381f39fd0b91b71090b8bd8922a26639
mahmutessiz <mahmutessiz71@gmail.com>
2023-10-28 11:46:30 +0300

    take data from supabase then add to the table

--- src/components/DashboardTransactions.vue
+++ src/components/DashboardTransactions.vue
@@ -1,59 +1,41 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import { supabase } from '../lib/supabaseClient'
+
+const transactions = ref([])
+
+async function getTransactions() {
+  const { data } = await supabase.from('budget_app').select()
+  transactions.value = data
+}
+
+onMounted(() => {
+  getTransactions()
+})
+</script>
 <template>
   <div class="w-full p-2 sm:p-4">
-    <div class="overflow-x-auto shadow dark:shadow-white/20">
-      <table class="table table-zebra">
+    <div class="overflow-x-auto shadow dark:shadow-white/20 h-96">
+      <table class="table table-zebra table-pin-rows">
         <!-- head -->
+        <caption class="text-xl">
+          Transactions
+        </caption>
         <thead>
           <tr class="hover dark:bg-green-900 dark:text-white/60">
             <th></th>
-            <th>Amount</th>
             <th>Category</th>
+            <th>Amount</th>
             <th>Date</th>
           </tr>
         </thead>
+        <!-- tbody -->
         <tbody>
-          <!-- row 1 -->
-          <tr class="hover">
-            <th>1</th>
-            <td>1525</td>
-            <td>Home</td>
-            <td>2023-10-16</td>
-          </tr>
-          <!-- row 2 -->
-          <tr class="hover">
-            <th>2</th>
-            <td>1525</td>
-            <td>Home</td>
-            <td>2023-10-16</td>
-          </tr>
-          <!-- row 3 -->
-          <tr class="hover">
-            <th>3</th>
-            <td>1525</td>
-            <td>Home</td>
-            <td>2023-10-16</td>
-          </tr>
-          <!-- row 4 -->
-          <tr class="hover">
-            <th>4</th>
-            <td>1525</td>
-            <td>Home</td>
-            <td>2023-10-16</td>
-          </tr>
-          <!-- row 5 -->
-          <tr class="hover">
-            <th>5</th>
-            <td>1525</td>
-            <td>Home</td>
-            <td>2023-10-16</td>
-          </tr>
-          <!-- row 6 -->
-          <tr class="hover">
-            <th>6</th>
-            <td>1525</td>
-            <td>Home</td>
-            <td>2023-10-16</td>
+          <tr class="hover" v-for="(transaction, index) in transactions" :key="index">
+            <th>{{ index + 1 }}</th>
+            <td>{{ transaction.category_name }}</td>
+            <td>{{ transaction.amount }}</td>
+            <td>{{ transaction.date }}</td>
           </tr>
         </tbody>
       </table>
