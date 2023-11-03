b98f552bf279fdcd5aca0570942f75fd297f7f14
mahmutessiz <mahmutessiz71@gmail.com>
2023-11-03 13:23:02 +0300

    add dropdown menu

--- src/components/DashBoardSidebar.vue
+++ src/components/DashBoardSidebar.vue
@@ -25,7 +25,16 @@
               d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
             />
           </svg>
-          <button class="btn" onclick="my_modal_2.showModal()">Add new</button>
+          <div class="dropdown">
+            <label tabindex="0" class="w-full">Add new</label>
+            <ul
+              tabindex="0"
+              class="dropdown-content z-[1] menu p-2 shadow-md shadow-black bg-base-100 rounded-box w-52"
+            >
+              <li><button onclick="my_modal_2.showModal()">Income</button></li>
+              <li><button>Transaction</button></li>
+            </ul>
+          </div>
         </a>
       </li>
       <li>
--- src/composables/addIncome.js
+++ src/composables/addIncome.js
@@ -1,10 +1,9 @@
 import { supabase } from '../lib/supabaseClient'
 
 async function useAddIncome(user_id, amount) {
-  const { data, error } = await supabase
+  const { error } = await supabase
     .from('income_transactions')
     .insert({ user_id: user_id, income: amount, date: new Date() })
-    .returns()
 
   if (error) {
     console.error('Error adding income: ', error)
