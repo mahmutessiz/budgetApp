d80103b162f0583bfae43c5d2d52b047f7263cd6
mahmutessiz <mahmutessiz71@gmail.com>
2023-11-08 13:55:51 +0300

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,5 @@
-<script setup>
-// The code is importing the `ref` and `onMounted` functions from the Vue library, as well as importing
-// the `supabase` object from the `supabaseClient` module located in the `../lib` directory. These
-// imports are used in the script setup to define reactive variables and perform actions when the
-// component is mounted.
-
-import { supabase } from '../lib/supabaseClient'
-
-const logout = async () => {
-  let { error } = await supabase.auth.signOut()
-  console.log('succesfull logout', error)
-  console.log(await supabase.auth.getUser())
-}
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <ul>
-      <button class="btn btn-error" @click="logout">tikla</button>
-    </ul>
-  </main>
+  <main class="w-full text-center min-h-[100vh]">Nothing here yet!</main>
 </template>
