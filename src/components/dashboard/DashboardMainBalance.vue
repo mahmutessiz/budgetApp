e552e25baa076585839a93e37af099b9f163b55d
mahmutessiz <mahmutessiz71@gmail.com>
2023-11-19 13:20:35 +0300

    add monthly total income

--- src/components/dashboard/DashboardMainBalance.vue
+++ src/components/dashboard/DashboardMainBalance.vue
@@ -1,3 +1,45 @@
+<script setup>
+import { useUserBalance } from '../../composables/getBalance.js'
+import { useGetSpendings } from '../../composables/getMonthlySpendingsTotal.js'
+import { useUserIncome } from '../../composables/getIncome.js'
+// Component import
+import AddIncomeForm from './AddIncomeForm.vue'
+import AddTransactionForm from './AddTransactionForm.vue'
+
+import { onMounted, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const userId = route.query.user
+const userBalance = ref() // User's balance
+const userSpending = ref() // User's spending
+
+const { balance, getUserBalance } = useUserBalance() // Get user's balance
+const monthlyIncomeTotal = ref() // User's monthly income total
+
+onMounted(async () => {
+  // Get the user's monthly income balance
+  const { getUserBalance: getUserIncomeMonthlyBalance } = useUserIncome()
+  const userIncome = await getUserIncomeMonthlyBalance(userId)
+  const userMonthlyIncomeTotal = ref(userIncome)
+  const currentMonth = new Date().getMonth()
+  monthlyIncomeTotal.value = userMonthlyIncomeTotal.value[currentMonth]
+
+  // Get the user's balance
+  await getUserBalance(userId)
+  userBalance.value = balance.value
+
+  // Get the user's spending
+  const spending = await useGetSpendings(userId)
+  userSpending.value = spending
+})
+
+watch(balance, (newBalance) => {
+  // Update the user's balance when it changes
+  userBalance.value = newBalance
+})
+</script>
+
 <template>
   <div class="w-full p-2 shadow dark:shadow-base-300 bg-base-200/50">
     <div
@@ -8,7 +50,9 @@
           <div class="stat-title text-base-200">Account balance</div>
           <div class="stat-value text-xl sm:text-4xl">${{ userBalance }}</div>
           <div class="stat-actions">
-            <button class="btn btn-sm bg-base-200">Add funds</button>
+            <button class="btn btn-sm bg-base-200" onclick="my_modal_4.showModal()">
+              Add funds
+            </button>
           </div>
         </div>
 
@@ -21,23 +65,21 @@
         </div>
 
         <div class="stat">
-          <div class="stat-title text-base-200">Total income</div>
-          <div class="stat-value text-xl sm:text-4xl">$89,400</div>
+          <div class="stat-title text-base-200" v-if="monthlyIncomeTotal">Total income</div>
+          <div class="stat-value text-xl sm:text-4xl">${{ monthlyIncomeTotal }}</div>
           <div class="stat-actions">
             <button class=""></button>
             <button class=""></button>
           </div>
         </div>
       </div>
-
-      <!-- <div class="text-2xl text-green-500 font-bold font-mono self-start ml-4" v-if="userBalance">
-        <span class="font-normal text-green-600 text-xl">Balance:</span><br />
-        <span class="font-normal text-green-600 text-2xl">$</span>{{ userBalance }}
+      <div class="stat" v-if="!monthlyIncomeTotal">
+        <div class="stat-title text-base-200">Total income</div>
+        <div class="stat-value text-xl sm:text-4xl">$ ******</div>
+        <div class="stat-actions">
+          <button class="btn btn-sm bg-base-200">Add funds</button>
+        </div>
       </div>
-      <div class="text-2xl text-green-500 font-bold font-mono self-start ml-4" v-else>
-        <span class="font-normal text-green-600 text-xl">Balance:</span><br />
-        <span class="font-normal text-green-600 text-2xl">$</span>*****
-      </div> -->
 
       <div class="stats w-[90%] sm:w-auto bg-rose-500 text-base-200">
         <div class="stat" v-if="userSpending">
@@ -46,7 +88,9 @@
             ${{ userSpending }} <span class="text-sm">/month</span>
           </div>
           <div class="stat-actions">
-            <button class="btn btn-sm bg-base-200">Add transaction</button>
+            <button class="btn btn-sm bg-base-200" onclick="my_modal_5.showModal()">
+              Add transaction
+            </button>
           </div>
         </div>
 
@@ -58,44 +102,26 @@
           </div>
         </div>
       </div>
-
-      <!-- <div class="text-2xl text-rose-500 font-bold font-mono self-start ml-4" v-if="userBalance">
-        <span class="font-normal text-rose-600 text-xl">Total spendings:</span><br />
-        <span class="font-normal text-rose-600 text-2xl">$</span>{{ userSpending
-        }}<span class="text-sm text-rose-600">/month</span>
-      </div>
-      <div class="text-2xl text-green-500 font-bold font-mono self-start ml-4" v-else>
-        <span class="font-normal text-rose-600 text-xl">Total spendings:</span><br />
-        <span class="font-normal text-rose-600 text-2xl">$</span>*****<span
-          class="text-sm text-rose-600"
-          >/month</span
-        >
-      </div> -->
     </div>
+    <!-- add income form -->
+    <dialog id="my_modal_4" class="modal">
+      <div class="modal-box">
+        <p class="py-4">Click outside to close</p>
+        <AddIncomeForm class="" />
+      </div>
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+    <!-- add transaction form -->
+    <dialog id="my_modal_5" class="modal">
+      <div class="modal-box">
+        <p class="py-4">Click outside to close</p>
+        <AddTransactionForm class="" />
+      </div>
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
   </div>
 </template>
-<script setup>
-import { useUserBalance } from '../../composables/getBalance.js'
-import { useGetSpendings } from '../../composables/getMonthlySpendingsTotal.js'
-import { onMounted, ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
-
-const route = useRoute()
-const userId = route.query.user
-const userBalance = ref()
-const userSpending = ref()
-
-const { balance, getUserBalance } = useUserBalance()
-
-onMounted(async () => {
-  await getUserBalance(userId)
-  userBalance.value = balance.value
-
-  const spending = await useGetSpendings(userId)
-  userSpending.value = spending
-})
-
-watch(balance, (newBalance) => {
-  userBalance.value = newBalance
-})
-</script>
