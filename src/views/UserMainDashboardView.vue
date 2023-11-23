6c1b9f07ae2155f5239f1a9b91364e0ef4cc38ff
mahmutessiz <mahmutessiz71@gmail.com>
2023-11-23 10:52:01 +0300

    remove unused component

--- src/views/UserMainDashboardView.vue
+++ src/views/UserMainDashboardView.vue
@@ -1,14 +1,13 @@
 <script setup>
 import DashboardMain from '../components/DashboardMain.vue'
 import DashBoardSidebar from '../components/dashboard/DashBoardSidebar.vue'
-import DashboardMobileMenu from '@/components/dashboard/DashboardMobileMenu.vue'
 </script>
+
 <template>
   <div>
     <div class="w-full sticky top-0">
       <DashBoardSidebar class="w-full" />
     </div>
     <DashboardMain />
-    <DashboardMobileMenu />
   </div>
 </template>
