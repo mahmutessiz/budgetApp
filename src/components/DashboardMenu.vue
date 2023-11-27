709ccb40475afa66c5f04fdca40a8697cbd51c7f
mahmutessiz <mahmutessiz71@gmail.com>
2023-11-27 12:01:57 +0300

    Go home reoute when logo clicked

--- src/components/DashboardMenu.vue
+++ src/components/DashboardMenu.vue
@@ -27,9 +27,9 @@ watchEffect(() => {
 
 <template>
   <header class="w-full flex justify-between p-4 items-center" v-if="!route.query.user">
-    <div class="md:pl-12">
+    <button @click="$router.push('/')" class="md:pl-12 cursor-pointer">
       <img src="../assets/logo.png" class="aspect-video w-24" alt="logo" />
-    </div>
+    </button>
     <div>
       <ul class="w-full md:flex justify-between gap-4 pr-12 hidden">
         <li>
